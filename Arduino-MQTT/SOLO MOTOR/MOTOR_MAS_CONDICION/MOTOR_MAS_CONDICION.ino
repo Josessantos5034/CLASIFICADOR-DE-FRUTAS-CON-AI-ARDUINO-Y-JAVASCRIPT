ceed6b4797f25df327fb8a21fb71825c66e74977
Josessantos5034 <josesantos50341202@gmail.com>
2020-11-17 16:44:13 -0600

    Son pruebas anteriores

--- Arduino-MQTT/SOLO MOTOR/MOTOR_MAS_CONDICION/MOTOR_MAS_CONDICION.ino
+++ Arduino-MQTT/SOLO MOTOR/MOTOR_MAS_CONDICION/MOTOR_MAS_CONDICION.ino
@@ -1,87 +1,66 @@
-#define STEP 19      // pin STEP de A4988 a pin 4
-#define DIR 18     // pin DIR de A4988 a pin 5
+#define posicion_manzana 0
+#define posicion_naranja 120
+#define posicion_pera 240
+
+int GradoActual = 0;
+int GradoDeseado = 0;
+
+const int P_Step = 19;
+const int P_Dir = 18;
+const int led_estado = 5;
+
 void setup() {
-pinMode(STEP, OUTPUT);  // pin 4 como salida
-pinMode(DIR, OUTPUT);   // pin 5 como salida
-Serial.begin(9600);
+  pinMode(P_Step, OUTPUT);
+  pinMode(P_Dir, OUTPUT);
+  pinMode(led_estado, OUTPUT);
+  digitalWrite(led_estado, 1);
+  Serial.begin(9600);
 }
 
 void loop() {
+  DetectarMensaje();
+  ActualizarPosionMotores();
+}
+
+void ActualizarPosionMotores() {
+
+  Serial.print(GradoActual);
+  Serial.print("-");
+  Serial.print(GradoDeseado);
+  Serial.println();
+  if (GradoActual < GradoDeseado) {
+    SubirGrado();
+  }
+  else if ( GradoActual > GradoDeseado) {
+    BajarGrado();
+  }
+
+}
+
+void SubirGrado() {
+  Serial.println("Moviendo Abajo");
+  GradoActual++;
+}
+
+void BajarGrado() {
+  Serial.println("Moviendo Abajo");
+  GradoActual--;
+}
+
+void DetectarMensaje() {
   if (Serial.available()) {
     char Letra = Serial.read();
-    if (Letra == 'M') { // MANZANA
-      digitalWrite(DIR, HIGH);    // giro en un sentido
-      for(int i = 0; i < 66; i++){     
-      digitalWrite(STEP, HIGH);       // nivel alto
-      delay(10);           //por 10 mseg
-      digitalWrite(STEP, LOW);        // nivel bajo
-      delay(10);          // por 10 mseg
+    digitalWrite(led_estado, 0);
+    if (Letra == 'M') {
+      GradoDeseado = posicion_manzana;
+      Serial.println("Cambiando a Manzana");
+    } else if (Letra == 'J') {
+      GradoDeseado = posicion_naranja;
+      Serial.println("Cambiando a Naranja");
+    } else if (Letra == 'P') {
+      GradoDeseado = posicion_pera;
+      Serial.println("Cambiando a Pera");
     }
-      delay(1000);          // demora de 2 segundos
-
-       digitalWrite(DIR, LOW);     // giro en sentido opuesto
-     for(int i = 0; i < 66; i++){
-       digitalWrite(STEP, HIGH); 
-       delay(10);
-       digitalWrite(STEP, LOW);
-       delay(10);
-    }
-      //delay(2000);
-    }
-    else if (Letra == 'J') { // NARANJA
-      digitalWrite(DIR, HIGH);    // giro en un sentido
-      for(int i = 0; i < 132; i++){     
-      digitalWrite(STEP, HIGH);       // nivel alto
-     // delay(10);          // por 10 mseg
-      digitalWrite(STEP, LOW);        // nivel bajo
-     // delay(10);          // por 10 mseg
-    }
-      //delay(2000);          // demora de 2 segundos
-
-      //digitalWrite(DIR, LOW);     // giro en sentido opuesto
-      //for(int i = 0; i < 132; i++){
-       //digitalWrite(STEP, HIGH); 
-       //delay(10);
-       //digitalWrite(STEP, LOW);
-       //delay(10);
-    //}
-      //delay(2000);
-    }else if (Letra == 'P') { // PERA
-     digitalWrite(DIR, HIGH);    // giro en un sentido
-      for(int i = 0; i < 198; i++){     
-      digitalWrite(STEP, HIGH);       // nivel alto
-      delay(10);          // por 10 mseg
-      digitalWrite(STEP, LOW);        // nivel bajo
-      delay(10);          // por 10 mseg
-    }
-      delay(2000);          // demora de 2 segundos
-
-      digitalWrite(DIR, LOW);     // giro en sentido opuesto
-      for(int i = 0; i < 198; i++){
-       digitalWrite(STEP, HIGH); 
-       delay(10);
-       digitalWrite(STEP, LOW);
-       delay(10);
-    }
-      delay(2000);
-    }else if (Letra == 'N') { //NADA
-      //digitalWrite(DIR, HIGH);    // giro en un sentido
-      //for(int i = 0; i < 200; i++){     
-      //digitalWrite(STEP, HIGH);       // nivel alto
-      //delay(10);          // por 10 mseg
-      //digitalWrite(STEP, LOW);        // nivel bajo
-      //delay(10);          // por 10 mseg
-   // }
-     // delay(2000);          // demora de 2 segundos
-
-     digitalWrite(DIR, LOW);     // giro en sentido opuesto
-     // for(int i = 0; i < 66; i++){
-      // digitalWrite(STEP, HIGH); 
-       //delay(10);
-       digitalWrite(STEP, LOW);
-       //delay(10);
-    }
-      delay(100);
-    }
+    digitalWrite(led_estado, 1);
   }
-//}
+}
