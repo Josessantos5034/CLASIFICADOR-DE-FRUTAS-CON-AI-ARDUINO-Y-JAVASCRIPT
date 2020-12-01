3180c14972c4ba6cdf42819350a5539c5d8754f9
Josessantos5034 <josesantos5034102@gmail.com>
2020-12-01 14:24:00 -0600

    cambio de las frutas que se usaran y de sus posiciones y sus leds

--- Arduino-MQTT/MOTOR/MOTOR_MAS_CONDICION/MOTOR_MAS_CONDICION.ino
+++ Arduino-MQTT/MOTOR/MOTOR_MAS_CONDICION/MOTOR_MAS_CONDICION.ino
@@ -38,10 +38,10 @@ void Conectar() {
 
   client.subscribe("/Proyecto/CLASIFICAR");
 }
-#define posicion_manzana 0
-#define posicion_naranja 120
-#define posicion_pera 240
-
+#define posicion_mandarina 0
+#define posicion_uva_verde 120
+#define posicion_uva_roja 240
+posicion_uva_roja
 int GradoActual = 0;
 int GradoDeseado = 0;
 
@@ -51,18 +51,18 @@ const int BotonDerecha = 17;
 const int P_Step = 19;
 const int P_Dir = 18;
 const int led_estado = 13;
-const int led_pera = 12;
-const int led_naranja = 14;
-const int led_manzana = 27;
+const int led_mandarina = 12;
+const int led_uva_verde = 14;
+const int led_uva_roja = 27;
 
 
 void setup() {
   pinMode(P_Step, OUTPUT);
   pinMode(P_Dir, OUTPUT);
   pinMode(led_estado, OUTPUT);
-  pinMode(led_pera, OUTPUT);
-  pinMode(led_naranja, OUTPUT);
-  pinMode(led_manzana, OUTPUT);
+  pinMode(led_mandarina, OUTPUT);
+  pinMode(led_uva_verde, OUTPUT);
+  pinMode(led_uva_roja, OUTPUT);
   pinMode(BotonIzquierda, INPUT);
   pinMode(BotonDerecha, INPUT);
   Serial.begin(9600);
@@ -130,72 +130,46 @@ void MoverMotor(int Pasos) {
   }
 }
 
-void DetectarMensaje() {
-  if (Serial.available()) {
-    char Letra = Serial.read();
-    digitalWrite(led_estado, 0);
-    if (Letra == 'M') {
-      GradoDeseado = posicion_manzana;
-      Serial.println("Cambiando a Manzana");
-      digitalWrite(led_manzana, HIGH);
-      digitalWrite(led_pera, LOW);
-      digitalWrite(led_naranja, LOW);
-    }
-    else if (Letra == 'J') {
-      GradoDeseado = posicion_naranja;
-      Serial.println("Cambiando a Naranja");
-      digitalWrite(led_naranja, HIGH);
-      digitalWrite(led_pera, LOW);
-      digitalWrite(led_manzana, LOW);
-    } else if (Letra == 'P') {
-      GradoDeseado = posicion_pera;
-      Serial.println("Cambiando a Pera");
-      digitalWrite(led_pera, HIGH);
-      digitalWrite(led_naranja, LOW);
-      digitalWrite(led_manzana, LOW);
-    }
-  }
-}
 void ActualizarBotones() {
   if (digitalRead(BotonIzquierda)) {
     Serial.println("Reiniciando hacia la izquierda");
     MoverMotor(1);
     GradoActual = 0;
     GradoDeseado = 0;
-    digitalWrite(led_pera, LOW);
-    digitalWrite(led_naranja, LOW);
-    digitalWrite(led_manzana, LOW);
+    digitalWrite(led_mandarina, LOW);
+    digitalWrite(led_uva_verde, LOW);
+    digitalWrite(led_uva_roja, LOW);
   }
   if (digitalRead(BotonDerecha)) {
     Serial.println("Reiniciando hacia la derecha");
     MoverMotor(-1);
     GradoActual = 0;
     GradoDeseado = 0;
-    digitalWrite(led_pera, LOW);
-    digitalWrite(led_naranja, LOW);
-    digitalWrite(led_manzana, LOW);
+    digitalWrite(led_mandarina, LOW);
+    digitalWrite(led_uva_verde, LOW);
+    digitalWrite(led_uva_roja, LOW);
   }
 }
 void RecibirMQTT(String &topic, String &payload) {
   Serial.println("Recibio: " + topic + " - " + payload);
-  if (payload == "PERA") {
-    GradoDeseado = posicion_pera;
-    Serial.println("Detecta_Pera");
-    digitalWrite(led_pera, HIGH);
-    digitalWrite(led_naranja, LOW);
-    digitalWrite(led_manzana, LOW);
-  } else if (payload == "MANZANA") {
-    GradoDeseado = posicion_manzana;
-    Serial.println("Detecta_Manzana");
-    digitalWrite(led_pera, LOW);
-    digitalWrite(led_naranja, LOW);
-    digitalWrite(led_manzana, HIGH);
+  if (payload == "MANDARINA") {
+    GradoDeseado = posicion_mandarina;
+    Serial.println("Detecta_Mandarina");
+    digitalWrite(led_mandarina, HIGH);
+    digitalWrite(led_uva_roja, LOW);
+    digitalWrite(led_uva_verde, LOW);
+  } else if (payload == "UVA ROJA") {
+    GradoDeseado = posicion_uva_roja;
+    Serial.println("Detecta_uva_roja");
+    digitalWrite(led_mandarina, LOW);
+    digitalWrite(led_uva_verde, LOW);
+    digitalWrite(led_uva_roja, HIGH);
   }
-  else if (payload == "NARANJA") {
-   GradoDeseado = posicion_naranja;
-   Serial.println("Detecta_Naranja");
-   digitalWrite(led_pera, LOW);
-   digitalWrite(led_naranja, HIGH);
-   digitalWrite(led_manzana, LOW);
+  else if (payload == "UVA VERDE") {
+   GradoDeseado = posicion_uva_verde;
+   Serial.println("Detectar_uva_verde");
+   digitalWrite(led_mandarina, LOW);
+   digitalWrite(led_uva_verde, HIGH);
+   digitalWrite(led_uva_roja, LOW);
  }
 }
